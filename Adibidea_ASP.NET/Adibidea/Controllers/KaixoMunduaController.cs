f1df5c95c94085b0de1c87835ad11515daab428d
Markel Lomana <lomana.markel@uni.eus>
2022-11-21 12:00:04 +0100

    Sarrera ASP.NET

---
+++ Adibidea_ASP.NET/Adibidea/Controllers/KaixoMunduaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Adibidea.Controllers
+{
+    public class KaixoMunduaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ongiEtorri(string izena, int zenbat = 1)
+        {
+            ViewBag.Mezua = "Hello " + izena;
+            ViewBag.Zenbat = zenbat;
+            return View();
+        }
+    }
+}
